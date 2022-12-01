cad940bc8f1d15f1d43b5e0d0e8086f15a0d2705
supershinu <85888017+supershinu@users.noreply.github.com>
2022-12-01 19:18:54 +0530

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,44 @@
+import React,{Fragment} from 'react';
+import '../css/App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Signin from './Signin';
+import Signup from './Signup';
+import Products from './Products';
+import {Router, Route, Switch} from 'react-router-dom';
+import useProducts from './useProducts';
+import Help from './Help';
+import Offers from './Offers';
+import Homepage from './Homepage';
+import History from './History'
+import Checkout from './Checkout';
+import OrderConfirmation from './OrderConfirmation';
+import Orders from './Orders';
+
+function App() {
+  
+  const {filteredProducts, cartItems, onAddCart, onRemoveCart, handleChangeSort, handleSearch, search,userLoginChange,pwdLoginChange,login,signUp,cnfPwdLoginChange,user,logout,onCheckout,orders,buy} = useProducts();
+  
+  return (
+    <Fragment>
+      <Router history = {History} >
+       <Header title="Vegan" count={cartItems} user={user} logout={logout} buy={buy}/>
+          <Switch>
+            <Route path="/Vegan-online-vegetable-shopping-reactjs" exact render={()=> < Homepage buy={buy}/>} />
+            <Route path="/Vegan-online-vegetable-shopping-reactjs/signup" render={()=> < Signup userLoginChange={userLoginChange} pwdLoginChange={pwdLoginChange} cnfPwdLoginChange={cnfPwdLoginChange} signUp = {signUp} buy={buy}/>} />
+            <Route path="/Vegan-online-vegetable-shopping-reactjs/signin" render={()=> <Signin userLoginChange={userLoginChange} pwdLoginChange={pwdLoginChange} login={login} buy={buy}/>} />
+            <Route path="/Vegan-online-vegetable-shopping-reactjs/products" render={()=><Products products={filteredProducts} cartItems={cartItems} onAddCart={onAddCart} onRemoveCart={onRemoveCart} handleChangeSort={handleChangeSort} handleSearch={handleSearch} search={search} onCheckout={onCheckout}/>} />
+            <Route path="/Vegan-online-vegetable-shopping-reactjs/help" render={()=> <Help/>} />
+            <Route path="/Vegan-online-vegetable-shopping-reactjs/offers" render={()=> <Offers/>} />
+            <Route path="/Vegan-online-vegetable-shopping-reactjs/checkout" render={()=> <Checkout cartItems={cartItems} onAddCart={onAddCart} onRemoveCart={onRemoveCart}/>}/>
+            <Route path="/Vegan-online-vegetable-shopping-reactjs/orderConfirmation" render={() => <OrderConfirmation/>}/>
+            <Route Path="/Vegan-online-vegetable-shopping-reactjs/orders" render={()=> <Orders orders={orders}/>}/>
+          </Switch>
+            
+        <Footer/>
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Header = (props) => {
+  return(
+    <nav className="navbar">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="/Vegan-online-vegetable-shopping-reactjs">{props.title}</a>
+        </div>
+        <ul className="nav navbar-nav navbar-right">
+          <li className="active"><a onClick={()=>props.buy("products")}> <i className="glyphicon glyphicon-search icon"></i>Products</a></li>
+          <li ><a onClick={()=>props.buy("offers")}><i className="fa fa-percent icon"></i>Offers</a></li>
+          <li><a onClick={()=>props.buy("help")}><i className="fa fa-life-ring icon"></i> Help</a></li>
+          
+          {props.user.userId!=null?(<li className="dropdown">
+            <a href="#" className="dropdown-toggle" data-toggle="dropdown"><span className="glyphicon glyphicon-user icon"></span>{props.user.email!==null? props.user.email:"Hello, Sign in"}</a>
+            <ul className="dropdown-menu">
+              <li align="center" className="well">
+              {props.user.email===null?(<a onClick={()=>props.buy("signin")}><span className="glyphicon glyphicon-user icon"></span>Sign In</a>):''}
+                  <a onClick={()=>props.buy("profile")} > Profile</a>
+                  <a onClick={()=>props.buy("orders")} > Orders</a>
+                  <a onClick={()=>props.buy("favourites")} > Favourites</a>
+                  {props.user.email!==null?(<a onClick={props.logout} ><span className="glyphicon glyphicon-log-out"></span> Logout</a>):''}
+              </li>
+              </ul>
+            </li>):''}
+          <li><a onClick={()=>props.buy("checkout")}><i className="fa fa-cart-plus icon"></i>Cart <span>{props.count.length}</span> </a></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Header;
