49190922f0fdb49a8efccca7a3bb5bc315e6b5d0
supershinu <85888017+supershinu@users.noreply.github.com>
2022-12-01 19:17:10 +0530

    Add files via upload

---
+++ build/precache-manifest.84d4d5790b28ad59f34414b93d68710f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2355bcf24788ba4da2b52e2d23951cd2",
+    "url": "/Vegan-online-vegetable-shopping-reactjs/index.html"
+  },
+  {
+    "revision": "3470e74a9113f3719f1f",
+    "url": "/Vegan-online-vegetable-shopping-reactjs/static/css/main.89c165c6.chunk.css"
+  },
+  {
+    "revision": "ac0fd68b2a9abd71f312",
+    "url": "/Vegan-online-vegetable-shopping-reactjs/static/js/2.037ad77c.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/Vegan-online-vegetable-shopping-reactjs/static/js/2.037ad77c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3470e74a9113f3719f1f",
+    "url": "/Vegan-online-vegetable-shopping-reactjs/static/js/main.077d776d.chunk.js"
+  },
+  {
+    "revision": "38804e9b75dc848b1c88",
+    "url": "/Vegan-online-vegetable-shopping-reactjs/static/js/runtime-main.6c4047ee.js"
+  }
+]);
